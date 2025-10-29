36a535c7ef3bb385852013438895a0581b0eda3f
Oskar Bergmann <oskar.bergmanntineo512@education.nsw.gov.au>
2025-10-30 09:25:01 +1100

    Fixing Code

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 from weather import get_current_weather 
 from waitress import serve 
 
-app = Flask(__name__)
+app= Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
-if __name__ == "__main__": 
-    serve(app, host="0.0.0.0", port=80000)
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=8000)
+    
--- weather.py
+++ weather.py
@@ -1,23 +1,21 @@
 from dotenv import load_dotenv
-from pprint import pprint
+from os import getenv
 import requests
 import os
 
 load_dotenv()
 
 def get_current_weather(city="Kansas City"):
-    requests_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&untis=metric'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+    response = requests.get(request_url)
+    return response.json()
 
-    weather_data = requests.get(request_url).json()
-
-    return weather_data
-
-if __name__ == "__main__": 
-     print('\n*** Get Current Weather conditions ***\n')
-
-     city = input ("\n Please enter a city name:")
-
-     weather_data = get_current_weather(city)
-
-     print("\n")
-     pprint(weather_data)
+if __name__ == "__main__":
+    print("\n*** Get Current Weather Conditions *** \n\n")
+    
+    city = input("\n Please enter a city name: \n")
+    
+    weather_data = get_current_weather(city)
+    
+    print("\n")
+    print(weather_data)
